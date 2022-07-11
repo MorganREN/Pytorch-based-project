3668648dc3d1cc98c539bdedd09a887f02e02541
m33394mr <mohan.ren@student.manchester.ac.uk>
2022-07-11 17:28:06 +0100

    Learning pytorch model train

---
+++ 5. Model_project/04_model_train.py
@@ -0,0 +1,72 @@
+# -*- coding:utf-8 -*-
+"""
+Author: mohanren
+Date: 11//07//2022//
+"""
+import torch
+import torchvision
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from Mohan import *
+
+data_train = torchvision.datasets.CIFAR10('./data', download=True, train=True, transform=torchvision.transforms.ToTensor())
+data_test = torchvision.datasets.CIFAR10('./data', download=True, train=False, transform=torchvision.transforms.ToTensor())
+
+data_train_size = len(data_train)
+data_test_size = len(data_test)
+
+train_loader = DataLoader(dataset=data_train, batch_size=64)
+test_loader = DataLoader(dataset=data_test, batch_size=64)
+
+# Create nn model
+mohan = Mohan()
+
+# Create Loss Function (Classification)
+loss_fn = nn.CrossEntropyLoss()
+
+# Create Optimizer
+learning_rate = 1e-2
+optim = torch.optim.SGD(mohan.parameters(), lr=learning_rate)
+
+# Train Parameter set
+total_train_step = 0
+total_test_step = 0
+epoch = 10
+
+# Add tensorboard
+writer = SummaryWriter('./logs')
+
+for i in range(5):
+    print("---------- Current epoch: {} ----------".format(i+1))
+    # Train
+    for data in train_loader:
+        imgs, labels = data
+        outputs = mohan(imgs)
+        # Get the loss
+        loss = loss_fn(outputs, labels)
+        # Optimize the model
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+        total_train_step += 1
+        if total_train_step % 100 == 0:
+            writer.add_scalar('train_loss', loss.item(), total_train_step)
+            print("Training No. {}, Loss No. {} ".format(total_train_step, loss.item()))
+
+    # Test start
+    total_test_loss = 0
+    with torch.no_grad():
+        for data in test_loader:
+            imgs, labels = data
+            outputs = mohan(imgs)
+            loss = loss_fn(outputs, labels)
+            total_test_loss += loss
+    writer.add_scalar('test_loss', total_test_loss, total_test_step)
+    print("The total loss in the testing dataset is {}".format(total_test_loss))
+    total_test_step += 1
+
+writer.close()
+
+
+
---
+++ 5. Model_project/Mohan.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+"""
+Author: mohanren
+Date: 11//07//2022//
+"""
+from torch import nn
+
+# Build Neural Network
+class Mohan(nn.Module):
+    def __init__(self):
+        super(Mohan, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=5, stride=1, padding=2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 32, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Flatten(),
+            nn.Linear(64*4*4, 64),
+            nn.Linear(64, 10)
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
